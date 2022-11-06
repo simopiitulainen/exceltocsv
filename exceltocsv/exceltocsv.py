250c8dd4d8132caf62d93ea3174de9d4e9d13cdb
Simo Piitulainen <sipesapi@student.jyu.fi>
2022-11-06 17:01:18 +0200

    Add initial folder structure

---
+++ exceltocsv/exceltocsv.py
@@ -0,0 +1,11 @@
+def read_excel(excel_filepath):
+    """
+    Reads specified rows/columns from a excel file to a pandas dataframe.
+    Args:
+        excel_filepath (str): path to the excel file 
+    Returns:
+        dataframe containing specified rows / columns
+             
+    """
+    print(f"I'll read {excel_filepath} contents for you some day and return a neat dataframe.")
+    
